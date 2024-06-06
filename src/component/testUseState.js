68a0f43ce34a3ee516a171bfcba6ad278ba0c16a
AiaLans <aiaulimfaizullaeva@gmail.com>
2024-06-07 01:08:01 +0500

    Created file with useState hook

---
+++ src/component/testUseState.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function Component () {
+  const getInitialState = () => true
+const [visible, setVisibility] = useState(getInitialState(false))
+  const handleCLick = () => {
+    setVisibility((currentValue) => !currentValue)
+  }
+  return (
+    <div className="container">
+      <h1>What is React?</h1>
+
+      <Button variant="primary" onClick="handleCLick">Show</Button>
+      {visible && <Card>
+          <Card.Body>A JavaScript library with virtual DOM</Card.Body>
+        </Card>
+      }
+    </div>
+  )
+}
